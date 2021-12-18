29ea5cfd632ba283a2e89c3aeb214afc41394227
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2021-12-18 00:48:48 -0300

    refatoração da lógica

--- main.js
+++ main.js
@@ -1,77 +1,56 @@
 var logDeInformacoes = document.querySelector('.log-de-informacoes')
 var campos = document.querySelectorAll(".campo")
-var player1 = {boleano:true, listasIds: [], nome: "Player1", status: ""}
-var player2 = {boleano:false, listasIds: [], nome: "Player2", status: ""}
+var player1 = {boleano: true, listaIds: [], nome: "Player1", status: ""}
+var player2 = {boleano: false, listaIds: [], nome: "Player2", status: ""}
 var gameOver = false
 
-function ligaJogo(player1, player2){
-    if(gameOver == false){
-        var jogador = rodaJogo(player1, player2)
-    }
-}
-
-function rodaJogo(player1, player2){
+function setLogDeInformacoes(){
     if(player1.boleano && !player2.boleano){
-        logDeInformacoes.textContent = "Rodada: player 1"
-        var resultado = marcaCampo(player1)
-        player1.boleano = resultado.boleano
-        player2.listasIds.push(...resultado.ids)
-        var vitoria = verificaVitoria(player1)
-        if(vitoria.status == "ganhou"){
-            logDeInformacoes.style.backgroundColor = "green"
-            logDeInformacoes.textContent = vitoria.nome + " venceu"
-            //fazer aqui as demais ações de gameover
-            gameOver = true
-        }
-        else{
-            player1 = vitoria
-            player2.boleano = true
-            return
-            rodaJogo(player1, player2)
-
-        }
+        logDeInformacoes.textContent = player1.nome + " joga"
     }else if(!player1.boleano && player2.boleano){
-        logDeInformacoes.textContent = "Rodada: player 2"
-        var resultado2 = marcaCampo(player2)
-        player2.boleano = resultado2.boleano
-        player2.listasIds.push(...resultado.ids)
-        var vitoria2 = verificaVitoria(player2)
-        if(vitoria2.status == "ganhou"){
-            logDeInformacoes.style.backgroundColor = "green"
-            logDeInformacoes.textContent = vitoria2.nome + " venceu"
-            //fazer aqui as demais ações de gameover
-            gameOver = true
-        }
-        else{
-            player2 = vitoria2
-            player1.boleano = true
-            rodaJogo(player1, player2)
+        logDeInformacoes.textContent = player2.nome + " joga"
+    } 
+}
 
-        }
+var rodaJogo = (e) =>{
+    let area = e.target
+    if(player1.boleano && !player2.boleano){
+        area.classList.add('adicionaX')
+        let id = parseInt(area.id)
+        player1.listaIds.push(id)
+        console.log(player1.listaIds)
+        player1.boleano = !player1.boleano
+        player2.boleano = !player2.boleano
+        let gameOver = validaJogo()
+        if(gameOver ? reinicia() : setLogDeInformacoes()) {}
+    }else if(!player1.boleano && player2.boleano){
+        area.classList.add('adicionaO')
+        let id = parseInt(area.id)
+        player2.listaIds.push(id)
+        console.log(player2.listaIds)
+        player1.boleano = !player1.boleano
+        player2.boleano = !player2.boleano
+        let gameOver = validaJogo()
+        if(gameOver ? reinicia() : setLogDeInformacoes()) {}    
     }
 }
-var area = null
 
-var addClass = (e) =>{
-    area = e.target
-    campos.forEach(function(campo){
-        campo.removeEventListener("click", addClass)
-    })
-    console.log(area)
-
+function validaJogo(){
+    if(player1.listaIds.length + player2.listaIds.length == 9){
+        logDeInformacoes.textContent = "Game Over"
+        console.log("Veio pra cá")
+        return true
+    }else{
+        return false
+    }
 }
 
+campos.forEach(function(campo){
+    campo.addEventListener("click", rodaJogo)
+})
 
-function marcaCampo (player){
-    campos.forEach(function(campo){
-        campo.addEventListener("click", addClass)
-
-    })
+function reinicia(){
+    console.log("reinicia")
 }
 
-function verificaVitoria(player){ 
-    player.status = ""
-    return player
-}
-
-ligaJogo(player1, player2)
+setLogDeInformacoes()
