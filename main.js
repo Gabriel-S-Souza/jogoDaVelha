897389891726758e2c8cfa63b5eec47394e4d57f
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-01-18 15:07:48 -0300

    Formatação da função winValidation

--- main.js
+++ main.js
@@ -38,8 +38,8 @@ var rodaJogo = (e) => {
 function winValidation(player) {
     let playerWin
     const possiveisVitorias = [
-        [0, 1, 2], [0, 3, 6],[0, 4, 8],[2, 4, 6],
-        [2, 5, 8],[3, 4, 5],[6, 7, 8],[1, 4, 7]
+        [0, 1, 2], [0, 3, 6], [0, 4, 8], [2, 4, 6],
+        [2, 5, 8], [3, 4, 5], [6, 7, 8], [1, 4, 7]
     ]
     possiveisVitorias.forEach(function(possivelVitoria) {
         let count = 0
