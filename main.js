d745b9390288f231ab2e3933d9707c575c1af421
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-01-18 04:48:38 -0300

    Refatorado

--- main.js
+++ main.js
@@ -1,89 +1,80 @@
 var logDeInformacoes = document.querySelector('.log-de-informacoes')
 var campos = document.querySelectorAll(".campo")
-var player1 = {boleano: true, listaIds: [], nome: "Player 1"}
-var player2 = {boleano: false, listaIds: [], nome: "Player 2"}
+var player1 = { boleano: true, listaIds: [], nome: "Player 1" }
+var player2 = { boleano: false, listaIds: [], nome: "Player 2" }
 
-function setLogDeInformacoes(){
+function setLogDeInformacoes() {
     logDeInformacoes.classList.remove("animateWin")
     logDeInformacoes.classList.remove("animateGameOver")
-    if(player1.boleano && !player2.boleano){
+    if (player1.boleano && !player2.boleano) {
         logDeInformacoes.textContent = player1.nome + " joga agora"
-    }else if(!player1.boleano && player2.boleano){
+    } else if (!player1.boleano && player2.boleano) {
         logDeInformacoes.textContent = player2.nome + " joga agora"
-    } 
+    }
 }
 
-var rodaJogo = (e) =>{
+var rodaJogo = (e) => {
     var area = e.target
     area.removeEventListener("click", rodaJogo)
-    if(player1.boleano && !player2.boleano){
+    if (player1.boleano && !player2.boleano) {
         area.classList.add('adicionaX')
         let id = parseInt(area.id)
         player1.listaIds.push(id)
         player1.boleano = !player1.boleano
         player2.boleano = !player2.boleano
-        let gameOver = validaJogo(player1)
-        if(gameOver ? reinicia() : setLogDeInformacoes()) {}
-    }else if(!player1.boleano && player2.boleano){
+        let gameOver = winValidation(player1)
+        if (gameOver ? reinicia() : setLogDeInformacoes()) {}
+    } else if (!player1.boleano && player2.boleano) {
         area.classList.add('adicionaO')
         let id = parseInt(area.id)
         player2.listaIds.push(id)
         player1.boleano = !player1.boleano
         player2.boleano = !player2.boleano
-        let gameOver = validaJogo(player2)
-        if(gameOver ? reinicia() : setLogDeInformacoes()) {}    
+        let gameOver = winValidation(player2)
+        if (gameOver ? reinicia() : setLogDeInformacoes()) {}
     }
 }
 
-function validaJogo(player){
-    if(player.listaIds.includes(0) && player.listaIds.includes(1) && player.listaIds.includes(2)){
-        colorWinner(0, 1, 2, player)
-        return true 
-    }else if(player.listaIds.includes(0) && player.listaIds.includes(3) && player.listaIds.includes(6)){
-        colorWinner(0, 3, 6, player)
-        return true
-    }else if(player.listaIds.includes(0) &&  player.listaIds.includes(4) && player.listaIds.includes(8)){
-        colorWinner(0, 4, 8, player)
-        return true
-    }else if(player.listaIds.includes(2) && player.listaIds.includes(4) && player.listaIds.includes(6)){
-        colorWinner(2, 4, 6, player)
-        return true
-    }else if(player.listaIds.includes(2) && player.listaIds.includes(5) && player.listaIds.includes(8)){
-        colorWinner(2, 5, 8, player)
-        return true
-    }else if(player.listaIds.includes(3) && player.listaIds.includes(4) && player.listaIds.includes(5)){
-        colorWinner(3, 4, 5, player)
-        return true
-    }else if(player.listaIds.includes(6) && player.listaIds.includes(7) && player.listaIds.includes(8)){
-        colorWinner(6, 7, 8, player)
-        return true
-    }else if(player.listaIds.includes(1) && player.listaIds.includes(4) && player.listaIds.includes(7)){
-        colorWinner(1, 4, 7, player)
-        return true
-    }
+function winValidation(player) {
+    let playerWin
+    const possiveisVitorias = [
+        [0, 1, 2], [0, 3, 6],[0, 4, 8],[2, 4, 6],
+        [2, 5, 8],[3, 4, 5],[6, 7, 8],[1, 4, 7]
+    ]
+    possiveisVitorias.forEach(function(possivelVitoria) {
+        let count = 0
+        possivelVitoria.forEach(function(id) {
+            if (player.listaIds.includes(id)) {
+                count++
+                if (count == 3) {
+                    animateWinner(...possivelVitoria, player)
+                    playerWin = true
+                }
+            }
+        })
+    })
+    return playerWin ?? isGameOver()
+}
 
-    if(player1.listaIds.length + player2.listaIds.length == 9){
+function isGameOver() {
+    if (player1.listaIds.length + player2.listaIds.length == 9) {
         logDeInformacoes.textContent = "Game Over"
         logDeInformacoes.classList.add("animateGameOver")
         return true
-    }else{
-        return false
-    }
+    } else return false
 }
 
-campos.forEach(function(campo){
+campos.forEach(function(campo) {
     campo.addEventListener("click", rodaJogo)
 })
 
-function reinicia(){
-    campos.forEach(function(campo){
+function reinicia() {
+    campos.forEach(function(campo) {
         campo.removeEventListener("click", rodaJogo)
-
     })
-    setTimeout(function(){
+    setTimeout(function() {
         setLogDeInformacoes()
-        rodaJogo
-        campos.forEach(function(campo){
+        campos.forEach(function(campo) {
             campo.classList.remove("adicionaX")
             campo.classList.remove("adicionaXred")
             campo.classList.remove("adicionaO")
@@ -92,20 +83,15 @@ function reinicia(){
             player1.listaIds = []
             player2.listaIds = []
         })
-    },1500)
-    
+    }, 1500)
 }
 
-function colorWinner(i, j, k, player){
+function animateWinner(i, j, k, player) {
     let camposVitoria = [campos[i], campos[j], campos[k]]
-    camposVitoria.forEach(function(campo){
-        if(campo.className.includes("adicionaX")){
-            campo.classList.remove("adicionaX")
-            campo.classList.add("adicionaXred")
-        }if(campo.className.includes("adicionaO")){
-            campo.classList.remove("adicionaO")
-            campo.classList.add("adicionaOred")
-        }
+    camposVitoria.forEach(function(campo) {
+        let hasX = campo.className.includes("adicionaX")
+        campo.classList.remove(hasX ? "adicionaX" : "adicionaO")
+        campo.classList.add(hasX ? "adicionaXred" : "adicionaOred")
     })
     logDeInformacoes.textContent = player.nome + " venceu"
     logDeInformacoes.classList.add("animateWin")
